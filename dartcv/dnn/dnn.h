9c70e4a87e993bfc112426c15638b5c7bd1c804b
rainy liu <rainyliusy3@gmail.com>
2024-10-17 23:17:52 +0800

    finish dnn

--- dartcv/dnn/dnn.h
+++ dartcv/dnn/dnn.h
@@ -9,7 +9,6 @@
 #ifndef _OPENCV3_DNN_H_
 #define _OPENCV3_DNN_H_
 
-
 #ifdef __cplusplus
 #include <opencv2/dnn.hpp>
 extern "C" {
@@ -29,63 +28,122 @@ CVD_TYPEDEF(void, Layer);
 CVD_TYPEDEF(void, AsyncArray);
 #endif
 
-CvStatus *AsyncArray_New(AsyncArray *rval);
-CvStatus *AsyncArray_Get(AsyncArray async_out, Mat out);
-CvStatus *Net_forwardAsync(Net net, const char *outputName, AsyncArray *rval);
-void      AsyncArray_Close(AsyncArrayPtr a);
+CvStatus* cv_dnn_AsyncArray_new(AsyncArray* rval);
+CvStatus* cv_dnn_AsyncArray_get(AsyncArray async_out, Mat out);
+CvStatus* cv_dnn_Net_forwardAsync(Net net, const char* outputName, AsyncArray* rval);
+void cv_dnn_AsyncArray_close(AsyncArrayPtr a);
 
-CvStatus *Net_Create(CVD_OUT Net *rval);
-CvStatus *Net_FromNet(Net net, CVD_OUT Net *rval);
-CvStatus *Net_ReadNet(const char *model, const char *config, const char *framework, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetBytes(const char *framework, VecUChar model, VecUChar config, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromCaffe(const char *prototxt, const char *caffeModel, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromCaffeBytes(VecUChar prototxt, VecUChar caffeModel, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromTensorflow(const char *model, const char *config, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromTensorflowBytes(VecUChar model, VecUChar config, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromTFLite(const char *model, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromTFLiteBytes(VecUChar bufferModel, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromTorch(const char *model, bool isBinary, bool evaluate, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromONNX(const char *model, CVD_OUT Net *rval);
-CvStatus *Net_ReadNetFromONNXBytes(VecUChar model, CVD_OUT Net *rval);
-void      Net_Close(NetPtr net);
+CvStatus* cv_dnn_Net_create(CVD_OUT Net* rval);
+CvStatus* cv_dnn_Net_fromNet(Net net, CVD_OUT Net* rval, CvCallback_0 callback);
+CvStatus* cv_dnn_Net_readNet(
+    const char* model,
+    const char* config,
+    const char* framework,
+    CVD_OUT Net* rval,
+    CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetBytes(
+    const char* framework, VecUChar model, VecUChar config, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromCaffe(
+    const char* prototxt, const char* caffeModel, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromCaffeBytes(
+    VecUChar prototxt, VecUChar caffeModel, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromTensorflow(
+    const char* model, const char* config, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromTensorflowBytes(
+    VecUChar model, VecUChar config, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromTFLite(const char* model, CVD_OUT Net* rval, CvCallback_0 callback);
+CvStatus* cv_dnn_Net_readNetFromTFLiteBytes(
+    VecUChar bufferModel, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromTorch(
+    const char* model, bool isBinary, bool evaluate, CVD_OUT Net* rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_readNetFromONNX(const char* model, CVD_OUT Net* rval, CvCallback_0 callback);
+CvStatus* cv_dnn_Net_readNetFromONNXBytes(VecUChar model, CVD_OUT Net* rval, CvCallback_0 callback);
+void cv_dnn_Net_close(NetPtr net);
 
-CvStatus *Net_BlobFromImage(Mat image, CVD_OUT Mat blob, double scalefactor,
-    CvSize size, Scalar mean,
-                            bool swapRB, bool crop, int ddepth);
-CvStatus *Net_BlobFromImages(VecMat images, CVD_OUT Mat blob, double scalefactor,
-    CvSize size, Scalar mean,
-                             bool swapRB, bool crop, int ddepth);
-CvStatus *Net_ImagesFromBlob(Mat blob, CVD_OUT VecMat *rval);
-CvStatus *Net_Empty(Net net, CVD_OUT bool *rval);
-CvStatus *Net_Dump(Net net, CVD_OUT char **rval);
-CvStatus *Net_SetInput(Net net, Mat blob, const char *name);
-CvStatus *Net_Forward(Net net, const char *outputName, CVD_OUT Mat *rval);
-CvStatus *Net_ForwardLayers(Net net, CVD_OUT VecMat *outputBlobs, VecVecChar outBlobNames);
-CvStatus *Net_SetPreferableBackend(Net net, int backend);
-CvStatus *Net_SetPreferableTarget(Net net, int target);
-CvStatus *Net_GetPerfProfile(Net net, CVD_OUT int64_t *rval);
-CvStatus *Net_GetUnconnectedOutLayers(Net net, CVD_OUT VecI32 *rval);
-CvStatus *Net_GetLayerNames(Net net, CVD_OUT VecVecChar *rval);
-CvStatus *Net_GetInputDetails(Net net, CVD_OUT VecF32 *scales, CVD_OUT VecI32 *zeropoints);
+CvStatus* cv_dnn_Net_blobFromImage(
+    Mat image,
+    CVD_OUT Mat blob,
+    double scalefactor,
+    CvSize size,
+    Scalar mean,
+    bool swapRB,
+    bool crop,
+    int ddepth,
+    CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_blobFromImages(
+    VecMat images,
+    CVD_OUT Mat blob,
+    double scalefactor,
+    CvSize size,
+    Scalar mean,
+    bool swapRB,
+    bool crop,
+    int ddepth,
+    CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_imagesFromBlob(Mat blob, CVD_OUT VecMat* rval, CvCallback_0 callback);
+bool cv_dnn_Net_empty(Net net);
+CvStatus* cv_dnn_Net_dump(Net net, CVD_OUT char** rval);
+CvStatus* cv_dnn_Net_setInput(Net net, Mat blob, const char* name, CvCallback_0 callback);
+CvStatus* cv_dnn_Net_forward(
+    Net net, const char* outputName, CVD_OUT Mat rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_forwardLayers(
+    Net net, CVD_OUT VecMat* outputBlobs, VecVecChar outBlobNames, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_setPreferableBackend(Net net, int backend);
+CvStatus* cv_dnn_Net_setPreferableTarget(Net net, int target);
+CvStatus* cv_dnn_Net_getPerfProfile(
+    Net net, CVD_OUT int64_t* rval, VecF64* layersTimes, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_getUnconnectedOutLayers(Net net, CVD_OUT VecI32* rval, CvCallback_0 callback);
+CvStatus* cv_dnn_Net_getLayerNames(Net net, CVD_OUT VecVecChar* rval, CvCallback_0 callback);
+CvStatus* cv_dnn_Net_getInputDetails(
+    Net net, CVD_OUT VecF32* scales, CVD_OUT VecI32* zeropoints, CvCallback_0 callback
+);
 
-CvStatus *Net_GetBlobChannel(Mat blob, int imgidx, int chnidx, CVD_OUT Mat *rval);
-CvStatus *Net_GetBlobSize(Mat blob, CVD_OUT Scalar *rval);
+CvStatus* cv_dnn_Net_getBlobChannel(
+    Mat blob, int imgidx, int chnidx, CVD_OUT Mat rval, CvCallback_0 callback
+);
+CvStatus* cv_dnn_Net_getBlobSize(Mat blob, VecI32* rval);
 
-CvStatus *Net_GetLayer(Net net, int layerid, CVD_OUT Layer *rval);
-CvStatus *Layer_InputNameToIndex(Layer layer, const char *name, CVD_OUT int *rval);
-CvStatus *Layer_OutputNameToIndex(Layer layer, const char *name, CVD_OUT int *rval);
-CvStatus *Layer_GetName(Layer layer, CVD_OUT char **rval);
-CvStatus *Layer_GetType(Layer layer, CVD_OUT char **rval);
-void      Layer_Close(LayerPtr layer);
+CvStatus* cv_dnn_Net_getLayer(Net net, int layerid, CVD_OUT Layer* rval);
+CvStatus* cv_dnn_Layer_inputNameToIndex(Layer layer, const char* name, CVD_OUT int* rval);
+CvStatus* cv_dnn_Layer_outputNameToIndex(Layer layer, const char* name, CVD_OUT int* rval);
+CvStatus* cv_dnn_Layer_getName(Layer layer, CVD_OUT char** rval);
+CvStatus* cv_dnn_Layer_getType(Layer layer, CVD_OUT char** rval);
+void cv_dnn_Layer_close(LayerPtr layer);
 
-CvStatus *NMSBoxes(VecRect bboxes, VecF32 scores, float score_threshold, float nms_threshold,
-                   CVD_OUT VecI32 *indices);
-CvStatus *NMSBoxesWithParams(VecRect bboxes, VecF32 scores, const float score_threshold,
-                             const float nms_threshold, CVD_OUT VecI32 *indices, const float eta,
-                             const int top_k);
+CvStatus* cv_dnn_NMSBoxes(
+    VecRect bboxes,
+    VecF32 scores,
+    float score_threshold,
+    float nms_threshold,
+    CVD_OUT VecI32* indices,
+    CvCallback_0 callback
+);
+CvStatus* cv_dnn_NMSBoxes_1(
+    VecRect bboxes,
+    VecF32 scores,
+    const float score_threshold,
+    const float nms_threshold,
+    CVD_OUT VecI32* indices,
+    const float eta,
+    const int top_k,
+    CvCallback_0 callback
+);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //_OPENCV3_DNN_H_
+#endif  //_OPENCV3_DNN_H_
