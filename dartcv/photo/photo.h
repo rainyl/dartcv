93415a831fda23c25897d0053ce8b35d0648074d
rainy liu <rainyliusy3@gmail.com>
2024-10-18 21:12:23 +0800

    finish photo

--- dartcv/photo/photo.h
+++ dartcv/photo/photo.h
@@ -6,8 +6,8 @@
     Licensed: Apache 2.0 license. Copyright (c) 2024 Rainyl.
 */
 
-#ifndef _OPENCV3_PHOTO_H_
-#define _OPENCV3_PHOTO_H_
+#ifndef CVD_PHOTO_H_
+#define CVD_PHOTO_H_
 
 #ifdef __cplusplus
 #include <opencv2/photo.hpp>
@@ -26,47 +26,96 @@ CVD_TYPEDEF(void, MergeMertens);
 CVD_TYPEDEF(void, AlignMTB);
 #endif
 
-CvStatus *ColorChange(Mat src, Mat mask, Mat dst, float red_mul, float green_mul, float blue_mul);
+CvStatus* cv_colorChange(
+    Mat src,
+    Mat mask,
+    Mat dst,
+    float red_mul,
+    float green_mul,
+    float blue_mul,
+    CvCallback_0 callback
+);
 
-CvStatus *SeamlessClone(Mat src, Mat dst, Mat mask, CvPoint p, Mat blend, int flags);
+CvStatus* cv_seamlessClone(
+    Mat src, Mat dst, Mat mask, CvPoint p, Mat blend, int flags, CvCallback_0 callback
+);
 
-CvStatus *IlluminationChange(Mat src, Mat mask, Mat dst, float alpha, float beta);
+CvStatus* cv_illuminationChange(
+    Mat src, Mat mask, Mat dst, float alpha, float beta, CvCallback_0 callback
+);
 
-CvStatus *TextureFlattening(Mat src, Mat mask, Mat dst, float low_threshold, float high_threshold,
-                            int kernel_size);
+CvStatus* cv_textureFlattening(
+    Mat src,
+    Mat mask,
+    Mat dst,
+    float low_threshold,
+    float high_threshold,
+    int kernel_size,
+    CvCallback_0 callback
+);
 
-CvStatus *FastNlMeansDenoisingColoredMulti(VecMat src, Mat dst, int imgToDenoiseIndex,
-                                           int temporalWindowSize);
-CvStatus *FastNlMeansDenoisingColoredMultiWithParams(VecMat src, Mat dst, int imgToDenoiseIndex,
-                                                     int temporalWindowSize, float h, float hColor,
-                                                     int templateWindowSize, int searchWindowSize);
-CvStatus *FastNlMeansDenoising(Mat src, Mat dst);
-CvStatus *FastNlMeansDenoisingWithParams(Mat src, Mat dst, float h, int templateWindowSize,
-                                         int searchWindowSize);
-CvStatus *FastNlMeansDenoisingColored(Mat src, Mat dst);
-CvStatus *FastNlMeansDenoisingColoredWithParams(Mat src, Mat dst, float h, float hColor,
-                                                int templateWindowSize, int searchWindowSize);
+CvStatus* cv_fastNlMeansDenoisingColoredMulti(
+    VecMat src, Mat dst, int imgToDenoiseIndex, int temporalWindowSize, CvCallback_0 callback
+);
+CvStatus* cv_fastNlMeansDenoisingColoredMulti_1(
+    VecMat src,
+    Mat dst,
+    int imgToDenoiseIndex,
+    int temporalWindowSize,
+    float h,
+    float hColor,
+    int templateWindowSize,
+    int searchWindowSize,
+    CvCallback_0 callback
+);
+CvStatus* cv_fastNlMeansDenoising(Mat src, Mat dst, CvCallback_0 callback);
+CvStatus* cv_fastNlMeansDenoising_1(
+    Mat src, Mat dst, float h, int templateWindowSize, int searchWindowSize, CvCallback_0 callback
+);
+CvStatus* cv_fastNlMeansDenoisingColored(Mat src, Mat dst, CvCallback_0 callback);
+CvStatus* cv_fastNlMeansDenoisingColored_1(
+    Mat src,
+    Mat dst,
+    float h,
+    float hColor,
+    int templateWindowSize,
+    int searchWindowSize,
+    CvCallback_0 callback
+);
 
-CvStatus *MergeMertens_Create(MergeMertens *rval);
-CvStatus *MergeMertens_CreateWithParams(float contrast_weight, float saturation_weight, float exposure_weight,
-                                        MergeMertens *rval);
-CvStatus *MergeMertens_Process(MergeMertens b, VecMat src, Mat dst);
-void      MergeMertens_Close(MergeMertensPtr b);
+CvStatus* cv_createMergeMertens(MergeMertens* rval);
+CvStatus* cv_createMergeMertens_1(
+    float contrast_weight, float saturation_weight, float exposure_weight, MergeMertens* rval
+);
+CvStatus* cv_MergeMertens_process(MergeMertens b, VecMat src, Mat dst, CvCallback_0 callback);
+void cv_MergeMertens_close(MergeMertensPtr b);
 
-CvStatus *AlignMTB_Create(AlignMTB *rval);
-CvStatus *AlignMTB_CreateWithParams(int max_bits, int exclude_range, bool cut, AlignMTB *rval);
-CvStatus *AlignMTB_Process(AlignMTB b, VecMat src, VecMat *dst);
-void      AlignMTB_Close(AlignMTBPtr b);
+CvStatus* cv_createAlignMTB(AlignMTB* rval);
+CvStatus* cv_createAlignMTB_1(int max_bits, int exclude_range, bool cut, AlignMTB* rval);
+CvStatus* cv_AlignMTB_process(AlignMTB b, VecMat src, VecMat* dst, CvCallback_0 callback);
+void cv_AlignMTB_close(AlignMTBPtr b);
 
-CvStatus *DetailEnhance(Mat src, Mat dst, float sigma_s, float sigma_r);
-CvStatus *EdgePreservingFilter(Mat src, Mat dst, int filter, float sigma_s, float sigma_r);
-CvStatus *PencilSketch(Mat src, Mat dst1, Mat dst2, float sigma_s, float sigma_r, float shade_factor);
-CvStatus *Stylization(Mat src, Mat dst, float sigma_s, float sigma_r);
+CvStatus* cv_detailEnhance(Mat src, Mat dst, float sigma_s, float sigma_r, CvCallback_0 callback);
+CvStatus* cv_edgePreservingFilter(
+    Mat src, Mat dst, int filter, float sigma_s, float sigma_r, CvCallback_0 callback
+);
+CvStatus* cv_pencilSketch(
+    Mat src,
+    Mat dst1,
+    Mat dst2,
+    float sigma_s,
+    float sigma_r,
+    float shade_factor,
+    CvCallback_0 callback
+);
+CvStatus* cv_stylization(Mat src, Mat dst, float sigma_s, float sigma_r, CvCallback_0 callback);
 
-CvStatus *PhotoInpaint(Mat src, Mat mask, Mat dst, float inpaint_radius, int algorithm_type);
+CvStatus* cv_inpaint(
+    Mat src, Mat mask, Mat dst, float inpaint_radius, int algorithm_type, CvCallback_0 callback
+);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //_OPENCV3_PHOTO_H
+#endif  //CVD_PHOTO_H_
