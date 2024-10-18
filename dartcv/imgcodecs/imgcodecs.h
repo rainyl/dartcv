e0a9352e092143fcff98937e7e199f26dbfa72ad
rainy liu <rainyliusy3@gmail.com>
2024-10-18 19:38:19 +0800

    finish imgcodecs

--- dartcv/imgcodecs/imgcodecs.h
+++ dartcv/imgcodecs/imgcodecs.h
@@ -6,8 +6,8 @@
     Licensed: Apache 2.0 license. Copyright (c) 2024 Rainyl.
 */
 
-#ifndef _OPENCV3_IMGCODECS_H_
-#define _OPENCV3_IMGCODECS_H_
+#ifndef CVD_IMGCODECS_H_
+#define CVD_IMGCODECS_H_
 
 #ifdef __cplusplus
 #include <opencv2/imgcodecs.hpp>
@@ -17,16 +17,33 @@ extern "C" {
 #include "dartcv/core/types.h"
 #include <stdbool.h>
 
-CvStatus *Image_IMRead(const char *filename, int flags, CVD_OUT Mat *rval);
-CvStatus *Image_IMWrite(const char *filename, Mat img, CVD_OUT bool *rval);
-CvStatus *Image_IMWrite_WithParams(const char *filename, Mat img, VecI32 params, CVD_OUT bool *rval);
-CvStatus *Image_IMEncode(const char *fileExt, Mat img, CVD_OUT bool *success, CVD_OUT VecUChar *rval);
-CvStatus *Image_IMEncode_WithParams(const char *fileExt, Mat img, VecI32 params, CVD_OUT bool *success,
-                                    CVD_OUT VecUChar *rval);
-CvStatus *Image_IMDecode(VecUChar buf, int flags, CVD_OUT Mat *rval);
+bool cv_haveImageReader(const char* filename);
+bool cv_haveImageWriter(const char* filename);
+size_t cv_imcount(const char* filename, int flags);
+CvStatus* cv_imdecode(VecUChar buf, int flags, CVD_OUT Mat rval, CvCallback_0 callback);
+CvStatus* cv_imencode(
+    const char* fileExt,
+    Mat img,
+    CVD_OUT bool* success,
+    CVD_OUT VecUChar* rval,
+    CvCallback_0 callback
+);
+CvStatus* cv_imencode_1(
+    const char* fileExt,
+    Mat img,
+    VecI32 params,
+    CVD_OUT bool* success,
+    CVD_OUT VecUChar* rval,
+    CvCallback_0 callback
+);
+CvStatus* cv_imread(const char* filename, int flags, CVD_OUT Mat rval, CvCallback_0 callback);
+CvStatus* cv_imwrite(const char* filename, Mat img, CVD_OUT bool* rval, CvCallback_0 callback);
+CvStatus* cv_imwrite_1(
+    const char* filename, Mat img, VecI32 params, CVD_OUT bool* rval, CvCallback_0 callback
+);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //_OPENCV3_IMGCODECS_H_
+#endif  //CVD_IMGCODECS_H_
